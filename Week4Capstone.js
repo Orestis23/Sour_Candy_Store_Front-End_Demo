da1e60ba482260ab5ad167807f292203592c4ab7
Orestis Sinis <orestisinis@gmail.com>
2019-08-12 06:35:14 -0400

    This is my week 4 capstone commit

---
+++ Week4Capstone.js
@@ -0,0 +1,58 @@
+var name;
+var price;
+
+// <script>
+// var carName = "Volvo";
+// document.getElementById("demo").innerHTML = carName; 
+// </script>
+
+
+var sourPatchKids = {
+	document.getElementByID("Sour Patch Kids").innerHTML = sourPatchKids;
+	name : "Sour Patch Kids",
+	price : "$4.99",
+	getInfo : function () {
+		console.log("Hello, there!")
+	}
+};
+
+sourPatchKids.getInfo;
+
+var warHeads = {
+	name : "War Heads",
+	price : "$2.99",
+	getInfo : function () {
+		console.log("Hello, there!")
+	}
+};
+
+warHeads.getInfo;
+
+var smogBalls = {
+	name : "Smog Balls",
+	price : "$2.99",
+	getInfo : function () {
+		console.log("Hello, there!")
+	}
+};
+smogBalls.getInfo;
+
+var sourPunchStraws = {
+	name : "Sour Punch Straws",
+	price : "$.99",
+	getInfo : function () {
+		console.log("Hello, there!")
+	}
+};
+
+sourPunchStraws.getInfo;
+
+var sourSweettarts = {
+	name : "Sour Sweettarts",
+	price : "$1.99",
+	getInfo : function () {
+		console.log("Hello, there!")
+	}
+};
+
+sourSweettarts.getInfo;
